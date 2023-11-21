edf0b94f71e8c1889e14c604cd612b08d939708a
Marianocastillo <marianocastillo08@gmail.com>
2023-11-21 00:51:49 -0400

    Proyecto de Lista de Tarea con React y typescript

---
+++ src/components/ListaTarea.tsx
@@ -0,0 +1,19 @@
+import { Tarea } from "./Tarea";
+
+type ListaTarea = {
+  listaTareas: string[];
+  borrarTarea: (index: number) => void;
+};
+
+export const ListaTareas = ({ listaTareas, borrarTarea }: ListaTarea) => {
+  return (
+    <div className="taskList">
+      {listaTareas.map((tarea, index) => (
+        <Tarea
+          key={index}
+          tarea={tarea}
+          borrarTarea={() => borrarTarea(index)}></Tarea>
+      ))}
+    </div>
+  );
+};
---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { ListaTareas } from "./ListaTarea";
+
+export const TodoApp = () => {
+  const [nuevaTarea, setNuevaTarea] = useState<string>('')
+
+  const [listaTareas, setListaTareas] = useState<string[]>([])
+
+  const handleAddTask = () => {
+    if (nuevaTarea.trim() === "") return;
+    setListaTareas((tareaanteriores) => [...tareaanteriores, nuevaTarea]);
+    setNuevaTarea("");
+  };
+  const handleBorrarTarea = (index: number) => {
+    setListaTareas(tareas => tareas.filter((_,i) => i !== index))
+  };
+
+  return (
+    <div>
+      <h1> Lista de Tareas</h1>
+
+      <div>
+        <input
+          type="text"
+          value={nuevaTarea}
+          onChange={(e) => setNuevaTarea(e.target.value)}
+          placeholder="Nueva Tarea"
+        />
+
+        <div className="container">
+          <button onClick={handleAddTask}>Agregar Tarea</button>
+        </div>
+      </div>
+      <ListaTareas
+        listaTareas={listaTareas}
+        borrarTarea={handleBorrarTarea}
+      ></ListaTareas>
+    </div>
+  );
+}
